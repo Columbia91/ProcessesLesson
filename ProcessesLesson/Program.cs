7888a6ff3724a4931a45b678595093ef5907ea7e
Columbia91 <alpha.z91@mail.ru>
2019-05-31 20:41:47 +0600

    Добавьте файлы проекта.

---
+++ ProcessesLesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProcessesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentProcess = Process.GetCurrentProcess();
+            Console.WriteLine($"{currentProcess.Id}, {currentProcess.StartTime}, {currentProcess.ProcessName}");
+
+            var chromeProcess = Process.GetProcessesByName("chrome");
+            foreach (var process in chromeProcess)
+            {
+                process.Kill();
+            }
+
+            //var calculatorProcess = Process.Start("calc");
+            //Console.ReadLine();
+            //calculatorProcess.Kill();
+            //Process.Start("https://mail.ru");
+        }
+    }
+}
